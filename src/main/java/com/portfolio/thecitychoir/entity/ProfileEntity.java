6179d9a63e24ffc9d89dd10bbb3c2c0babac0a55
TimiBolaji <mattybolaji@gmail.com>
2025-12-09 23:56:19 +0100

    added profile entity

---
+++ src/main/java/com/portfolio/thecitychoir/entity/ProfileEntity.java
@@ -0,0 +1,54 @@
+package com.portfolio.thecitychoir.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_profiles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String gender;
+    private String phone;
+    private String part;
+    private String password;
+
+    @Column(unique = true)
+    private String registrationNumber;
+
+    private Boolean isActive;
+    private String activationToken;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        if(this.isActive == null) {
+            isActive = false;
+        }
+    }
+
+}
