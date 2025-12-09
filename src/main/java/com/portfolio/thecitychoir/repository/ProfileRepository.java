7059ab45c62c08baf5629ba6c7adac5b125c7636
TimiBolaji <mattybolaji@gmail.com>
2025-12-09 23:57:07 +0100

    added profile repository

---
+++ src/main/java/com/portfolio/thecitychoir/repository/ProfileRepository.java
@@ -0,0 +1,20 @@
+package com.portfolio.thecitychoir.repository;
+
+import com.portfolio.thecitychoir.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+    Optional<ProfileEntity> findByEmail(String email);
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
+    boolean existsByEmail(String email);
+    Optional<ProfileEntity> findByRegistrationNumber(String registrationNumber);
+    Iterable<ProfileEntity> findByPart(String part);
+    long countByPart(String part);
+
+
+
+
+
+}
