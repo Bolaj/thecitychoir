566fbbfbef6020baa992c1eb7a1d9a59b1f8c3d6
TimiBolaji <mattybolaji@gmail.com>
2025-12-09 01:44:47 +0100

    Initial commit

---
+++ src/main/java/com/portfolio/thecitychoir/ThecitychoirApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.thecitychoir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecitychoirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThecitychoirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/thecitychoir/ThecitychoirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.thecitychoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThecitychoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
