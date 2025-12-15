7ad338e4f08e315ef5995ecbe0204769834c0ebd
TimiBolaji <mattybolaji@gmail.com>
2025-12-16 00:40:26 +0100

    updated repo

--- src/main/java/com/portfolio/thecitychoir/repository/RehearsalRepository.java
+++ src/main/java/com/portfolio/thecitychoir/repository/RehearsalRepository.java
@@ -3,5 +3,9 @@ package com.portfolio.thecitychoir.repository;
 import com.portfolio.thecitychoir.entity.RehearsalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface RehearsalRepository extends JpaRepository<RehearsalEntity, Long> {
+    boolean existsByRehearsalDate(LocalDate rehearsalDate);
+
 }
