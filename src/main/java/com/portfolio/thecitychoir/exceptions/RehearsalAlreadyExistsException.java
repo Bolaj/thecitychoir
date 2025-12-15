c45707c043266a7f2d9358140299915f6c013524
TimiBolaji <mattybolaji@gmail.com>
2025-12-16 00:40:03 +0100

    added exception handler

---
+++ src/main/java/com/portfolio/thecitychoir/exceptions/RehearsalAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.portfolio.thecitychoir.exceptions;
+
+public class RehearsalAlreadyExistsException extends RuntimeException {
+    public RehearsalAlreadyExistsException(String message) {
+        super(message);
+    }
+}
